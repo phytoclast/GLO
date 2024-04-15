f1ed730b21708afd1d1556c4929c0d002fc46a40
Greg Schmidt <greg.schmidt@mi.usda.gov>
2024-04-15 17:00:13 -0400

    Update model90.R

--- model90.R
+++ model90.R
@@ -16,6 +16,7 @@ pts <- readRDS('points/pts.geo1.RDS')
 pts.vars <- readRDS('pts.vars.RDS')
 vars90 <- rast('gis/vars90.tif')
 
+
 # vars270 <- vars90 |> aggregate(fact=3, fun="mean", na.rm=T)
 # writeRaster(vars270,'gis/vars270.tif', overwrite=T)
 # pts.vars90 <- pts |> vect() |> project(vars)
@@ -27,7 +28,9 @@ vars90 <- rast('gis/vars90.tif')
 # pts.vars270 <- pts |> cbind(pts.vars270)
 # saveRDS(pts.vars270, 'pts.vars270.RDS')
 vars270 <- rast('gis/vars270.tif')
-
+# pca90 <- princomp(vars270, cor = T, maxcell=ncell(vars270)*0.1)
+# pca90grid <- predict(pca90, pca90)
+# writeRaster(pca90grid,'gis/pca90grid.tif', overwrite=T)
 pts.vars90 <- readRDS('pts.vars90.RDS')
 pts.vars270 <- readRDS('pts.vars270.RDS')
 pts.vars90 <- pts.vars90 |> mutate(Level2 = ifelse(Species %in% 'Thuja', Species, Level2))
