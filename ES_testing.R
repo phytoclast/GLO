3d925f99e9695327a8a48daa961d54aeba133b13
Greg Schmidt <greg.schmidt@mi.usda.gov>
2024-05-15 08:24:17 -0400

    Create ES_testing.R

---
+++ ES_testing.R
@@ -0,0 +1,34 @@
+library(sf)
+library(terra)
+library(dplyr)
+library(ggplot2)
+library(ranger)
+library(gam)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+MLRA <- st_read('gis/MLRA_2017.shp')
+pts <- readRDS('points/pts.geo1.RDS')
+
+
+pts.vars <- readRDS('pts.vars.RDS')
+vars90 <- rast('gis/vars90.tif')
+vars270 <- rast('gis/vars270.tif')
+# pca90 <- princomp(vars270, cor = T, maxcell=ncell(vars270)*0.1)
+# pca90grid <- predict(pca90, pca90)
+# writeRaster(pca90grid,'gis/pca90grid.tif', overwrite=T)
+pts.vars90 <- readRDS('pts.vars90.RDS')
+pts.vars270 <- readRDS('pts.vars270.RDS')
+pts.vars90 <- pts.vars90 |> mutate(Level2 = ifelse(Species %in% 'Thuja', Species, Level2))
+pts.vars270 <- pts.vars270 |> mutate(Level2 = ifelse(Species %in% 'Thuja', Species, Level2))
+ttt <- pts.vars90 |> group_by(Level2) |> summarize(npts = length(Level2))
+ttt <- subset(ttt, !is.na(Level2) & !Level2 %in% c('unk','no tree') & npts >= 10000)
+ttt <- ttt$Level2
+cors <- readRDS('output/cors.RDS')
+
+
+for(i in 1:length(ttt)){
+  x <- rast(paste0('gis/models90/',ttt[i],'.tif'))
+  assign(ttt[i], x)         };rm(x)
+
+Species <- rast(mget(ttt))
+
