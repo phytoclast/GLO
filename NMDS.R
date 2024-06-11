d4e1ce6d5d186fc9eb87c007ebab18281265f59f
Greg Schmidt <greg.schmidt@mi.usda.gov>
2024-06-11 17:05:17 -0400

    Create NMDS.R

---
+++ NMDS.R
@@ -0,0 +1,129 @@
+
+#NMDS ----
+library(vegan)
+set.seed(0)
+
+library(sf)
+library(terra)
+library(dplyr)
+library(ggplot2)
+library(ranger)
+library(vegan)
+library(cluster)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+MLRA <- st_read('gis/MLRA_2017.shp')
+pts <- readRDS('points/pts.geo1.RDS')
+
+
+pts.vars <- readRDS('pts.vars.RDS')
+vars90 <- rast('gis/vars90.tif')
+vars270 <- rast('gis/vars270.tif')
+# pca90 <- princomp(vars270, cor = T, maxcell=ncell(vars270)*0.1)
+# pca90grid <- predict(pca90, pca90)
+# writeRaster(pca90grid,'gis/pca90grid.tif', overwrite=T)
+pts.vars90 <- readRDS('pts.vars90.RDS')
+pts.vars270 <- readRDS('pts.vars270.RDS')
+pts.vars90 <- pts.vars90 |> mutate(Level2 = ifelse(Species %in% 'Thuja', Species, Level2))
+pts.vars270 <- pts.vars270 |> mutate(Level2 = ifelse(Species %in% 'Thuja', Species, Level2))
+ttt <- pts.vars90 |> group_by(Level2) |> summarize(npts = length(Level2))
+ttt <- subset(ttt, !is.na(Level2) & !Level2 %in% c('unk','no tree') & npts >= 10000)
+ttt <- ttt$Level2
+cors <- readRDS('output/cors.RDS')
+
+#make species raster stack ----
+for(i in 1:length(ttt)){
+  x <- rast(paste0('gis/models90/',ttt[i],'.tif'))
+  assign(ttt[i], x)         };rm(x)
+
+Species <- rast(mget(ttt))
+
+#make and extract sample points
+MLRA <- st_transform(MLRA, crs(Species))
+MLRA <- MLRA |> mutate(LRU = case_when(LRU %in% c('98A1','98A3','98A4','98A5') ~ '98A',
+                                       LRU %in% c('97A1','97A2') ~ '97A',
+                                       LRU %in% c('97B1','97B2') ~ '97B',
+                                       TRUE ~ LRU))
+
+MLRA94 <- subset(MLRA, MLRA %in% c("94A","94C"), select="LRU")
+plot(MLRA94)
+MLRA94.vect <- vect(MLRA94)
+spts <- terra::spatSample(MLRA94.vect, size = 1000)
+spts.mlra <- st_as_sf(spts) |> st_drop_geometry()
+spts.spp <- terra::extract(Species, spts)
+spts.env <- terra::extract(vars90, spts)
+
+spts.es <- cbind(spts.spp, spts.env[,-1], spts.mlra) |> subset(!is.na(pH50) & !is.na(Tsuga))
+
+#ES site key ---
+
+spts.es <- spts.es |> mutate(ES = case_when(rockdepth < 150 ~ 'Limestone',
+                                            floodfrq > 0 ~ 'Floodplain',
+                                            histic > 0.5 &  hydric >= 0.5 ~ 'Mucks',
+                                            sand150 >= 80 & sand50 >= 70 | sand50 >80 ~ 'Sandy',
+                                            TRUE ~'Loamy'))
+
+spts.es <- spts.es |> mutate(ES = case_when(ES %in%  'Floodplain' & hydric >= 0.5 ~ 'Wet Floodplain',
+                                            ES %in%  'Floodplain' ~ 'Moist Floodplain',
+                                            ES %in% 'Mucks' & pH50 >= 5 ~ "Euic Muck",
+                                            ES %in% 'Mucks'  ~ "Acidic Peat",
+                                            ES %in% 'Sandy' &  watertable > 100 ~ 'Dry Sand',
+                                            ES %in% 'Sandy' &  hydric < 0.5 ~ 'Moist Sand',
+                                            ES %in% 'Sandy' ~ 'Wet Sand',
+                                            ES %in% 'Loamy' &  watertable > 100 ~ 'Dry Loam',
+                                            ES %in% 'Loamy' &  hydric < 0.5 ~ 'Moist Loam',
+                                            ES %in% 'Loamy' ~ 'Wet Loam',
+                                            TRUE ~ ES))
+
+spts.es <- spts.es |> mutate(ES = case_when(ES %in%  'Dry Sand' & LRU %in% '94AA' & Bhs >= 0.5 & spodic >= 0.5 ~ 'Rich Sand',
+                                            ES %in%  'Dry Sand' & LRU %in% '94AA'  ~ 'Dry Sand',
+                                            ES %in%  'Dry Sand' & (spodic >= 0.5 | pH50 >= 5 | carbdepth < 100) ~ 'Rich Sand',
+                                            ES %in%  'Dry Sand'  ~ 'Dry Sand',
+                                            ES %in%  'Moist Sand'  & (spodic >= 0.5 | pH50 <= 5) ~ 'Acidic Moist Sand',
+                                            ES %in%  'Moist Sand'   ~ 'Euic Moist Sand',
+                                            ES %in%  'Wet Sand'  & (spodic >= 0.5 | pH50 <= 5.5) ~ 'Acidic Wet Sand',
+                                            ES %in%  'Wet Sand'   ~ 'Euic Wet Sand',
+                                            TRUE ~ ES))
+spts.es <- spts.es |> mutate(ES = ifelse(tan(slope)*100 >= 15 &  watertable > 100 & floodfrq <= 0 &  hydric < 0.5, paste(ES, "Slopes"), paste(ES, "Plains")))
+
+spts.es <- spts.es |> mutate(ESD = paste(LRU, ES))
+
+spts.es <- spts.es |> mutate(ESD = ifelse(rockdepth < 150, 'MLRA 94C Limestone Plains', ESD))
+
+colnames(spts.es)
+spts.es.spp <- spts.es |> select(all_of(ttt))
+spts.es.env <- spts.es |> select(c("p","m","e","s","d","Twh","Tw","Tc","Tcl","Tg",
+                                   "Tg30","Bhs","carbdepth","clay150","floodfrq","histic",
+                                   "humic","humicdepth","hydric","ksatdepth","OM150","pH50","rockdepth","sand150",
+                                   "sand50","spodic","watertable","elev","slope","aspect",
+                                   "slope500","shade","popen","nopen","solar"))
+spts.es.dist <- vegan::vegdist(spts.es.spp, method = 'bray', binary = F)
+spts.es.groups <- spts.es |> mutate(group = as.integer(as.factor(ESD))) |> select(c(ID, group, ESD))
+groups <- spts.es.groups |> select(c(group, ESD)) |> unique()
+dist_tbl <- as_tibble(spts.es.dist, rownames="samples")
+
+
+mds <- metaMDS(spts.es.dist)
+
+scores(mds) |> as_tibble(rownames='sample') |> mutate(sample=as.numeric(sample)) |>
+  inner_join(spts.es, by=join_by(sample==ID)) |> #mutate(class=ifelse(Quercus>0.5,1,0)) |>
+  ggplot(aes(x=NMDS1,y=NMDS2, color=LRU)) +
+  geom_point()
+
+
+nmds <- metaMDS(spts.es.spp)
+en <- envfit(mds, spts.es.env, na.rm = TRUE)
+
+scores(nmds)
+plot(en)
+
+sp.df <- scores(nmds, display='species') |> as_tibble(rownames='species')
+en.df <- scores(en, display='vectors')|> as_tibble(rownames='vectors')
+
+ggplot() +
+  geom_point(data=sp.df, aes(x=NMDS1,y=NMDS2))+
+  geom_text(data=sp.df, aes(label=species, x=NMDS1,y=NMDS2), vjust = 0, nudge_y = 0.02, nudge_x = 0.05, color='blue')+
+  geom_segment(data=en.df, aes(x=0,y=0,xend=NMDS1,yend=NMDS2), arrow = arrow(length = unit(0.03, "npc")), color='red')+
+  geom_text(data=en.df, aes(label=vectors, x=NMDS1,y=NMDS2), vjust = 0, nudge_y = 0.02, nudge_x = 0.05, color='red')
+
+
